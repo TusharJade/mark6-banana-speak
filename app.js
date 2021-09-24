f395dc6c3eddf00d1ac7360807f321c9a4331503
TusharJade <79801546+TusharJade@users.noreply.github.com>
2021-09-25 00:14:32 +0530

    queryselector callback learned

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-var username = prompt("What is your name?");
-
-var welcomeMessege = ("welcome to the website " + username);
-
-alert(welcomeMessege);
-
-console.log(username);
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputdiv = document.querySelector("#output");
 
 
+var btn = function act(){
+    console.log("clicked!");
+    console.log(txtInput.value);
+}
 
+btnTranslate.addEventListener("click" , btn);
